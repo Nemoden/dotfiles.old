0dfc7371851ff29a170e0ac427689eb545813023
Kirill "Nemoden" K <i@nemoden.com>
2012-09-25 23:05:21 +1100

    Installed command-t and surround as submodule bundles

--- bootstrap.py
+++ bootstrap.py
@@ -8,7 +8,7 @@ homedir = os.path.expanduser('~')
 # exchange information with user
 answer = None
 for filename in os.listdir(dotfiles_path):
-  if filename ==  __file__:
+  if filename in  [__file__, '.gitmodules']:
     continue
   else:
     pass
